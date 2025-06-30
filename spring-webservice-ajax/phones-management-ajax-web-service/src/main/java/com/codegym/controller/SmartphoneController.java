afcc181739a32a7944f8d30729b1deedfa4bf7b6
hoangtuanc02 <tuan4695hatinh@gmail.com>
2025-06-30 21:49:47 +0700

    thuc hanh 2 bai 12

---
+++ spring-webservice-ajax/phones-management-ajax-web-service/src/main/java/com/codegym/controller/SmartphoneController.java
@@ -0,0 +1,38 @@
+package com.codegym.controller;
+
+import com.codegym.model.Smartphone;
+import com.codegym.service.ISmartphoneService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/api/smartphones")
+public class SmartphoneController {
+    @Autowired
+    private ISmartphoneService smartphoneService;
+
+    @GetMapping
+    public ResponseEntity<Iterable<Smartphone>> listSmartphones() {
+        return new ResponseEntity<>(smartphoneService.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Smartphone> createSmartphone(@RequestBody Smartphone smartphone) {
+        return new ResponseEntity<>(smartphoneService.save(smartphone), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Smartphone> deleteSmartphone(@PathVariable Long id) {
+        Optional<Smartphone> smartphoneOptional = smartphoneService.findById(id);
+        if (!smartphoneOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        smartphoneService.remove(id);
+        return new ResponseEntity<>(smartphoneOptional.get(), HttpStatus.NO_CONTENT);
+    }
+}
---
+++ spring-webservice-ajax/phones-management-ajax-web-service/src/main/java/com/codegym/service/impl/SmartphoneService.java
@@ -0,0 +1,35 @@
+package com.codegym.service.impl;
+
+import com.codegym.model.Smartphone;
+import com.codegym.repository.ISmartphoneRepository;
+import com.codegym.service.ISmartphoneService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class SmartphoneService implements ISmartphoneService {
+    @Autowired
+    private ISmartphoneRepository iSmartphoneRepository;
+
+    @Override
+    public Iterable<Smartphone> findAll() {
+        return iSmartphoneRepository.findAll();
+    }
+
+    @Override
+    public Optional<Smartphone> findById(Long id) {
+        return iSmartphoneRepository.findById(id);
+    }
+
+    @Override
+    public Smartphone save(Smartphone smartPhone) {
+        return iSmartphoneRepository.save(smartPhone);
+    }
+
+    @Override
+    public void remove(Long id) {
+        iSmartphoneRepository.deleteById(id);
+    }
+}
