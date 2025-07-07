c5a2d56f0d96574f878816aac583fe6d94f90b71
hoangtuanc02 <tuan4695hatinh@gmail.com>
2025-07-07 16:21:37 +0700

    thuc hanh 2 bai 15

---
+++ spring-security-jwt/src/main/java/com/codegym/configuration/AppConfiguration.java
@@ -0,0 +1,12 @@
+package com.codegym.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.codegym")
+public class AppConfiguration implements WebMvcConfigurer {
+}
